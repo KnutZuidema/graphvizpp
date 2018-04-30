1a11d0ba46c48ba09f1a2567510ccd4c57b01cef
Knut Zuidema <knut.zuidema@gmail.com>
2018-05-01 01:00:05 +0200

    Initial commit

---
+++ library.h
@@ -0,0 +1,29 @@
+#ifndef GRAPHVIZPP_LIBRARY_H
+#define GRAPHVIZPP_LIBRARY_H
+
+#include <string>
+#include <vector>
+
+
+namespace graphivizpp{
+    class Graph{
+
+        class Statement{
+
+            enum Attribute{
+                Graph,
+                Node,
+                Edge;
+            };
+
+            std::vector statement_list;
+        };
+        bool directed = false;
+        bool strict = false;
+        std::string id = "";
+
+    };
+}
+
+
+#endif
