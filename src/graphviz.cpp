dd9e96f87ddd4ff9910fc9f46c89a9e2dddfa6df
Knut Zuidema <knut.zuidema@gmail.com>
2018-05-06 23:43:13 +0200

    added functionality and gitignore

--- src/graphviz.cpp
+++ src/graphviz.cpp
@@ -1,1 +1,59 @@
+#include <sstream>
+#include <fstream>
+#include <utility>
 #include "graphviz.h"
+
+using namespace graphvizpp;
+
+Node *Graph::add_node(std::string id) {
+    auto node = new Node(std::move(id));
+    nodes.push_back(node);
+    return node;
+}
+
+Edge *Graph::add_edge(std::string from, std::string to) {
+    auto edge = new Edge(std::move(from), std::move(to));
+    edges.push_back(edge);
+    return edge;
+}
+
+std::string Graph::to_string() {
+    std::stringstream out;
+    out << (strict ? "strict " : "")
+        << (directed ? "digraph" : "graph")
+        << "{\n";
+    for(const auto &attribute : graph_attributes){
+        out << '\t' << attribute.first << '=' << attribute.second << '\n';
+    }
+    out << '\t' << "node [ ";
+    for(const auto &attribute : node_attributes){
+        out << attribute.first << '=' << attribute.second << ' ';
+    }
+    out << "]\n"
+        << '\t' << "edge [ ";
+    for(const auto &attribute : edge_attributes){
+        out << attribute.first << '=' << attribute.second << ' ';
+    }
+    out << "]\n";
+    for(auto &node : nodes){
+        out << '\t' << node->id << " [ ";
+        for(const auto &attribute : node->attributes){
+            out << attribute.first << '=' << attribute.second << ' ';
+        }
+        out << "]\n";
+    }
+    for(auto &edge : edges){
+        out << '\t' << edge->from << (directed ? " -> " : " -- ") << edge->to << " [ ";
+        for(const auto &attribute : edge->attributes){
+            out << attribute.first << '=' << attribute.second << ' ';
+        }
+        out << "]\n";
+    }
+    out << '}';
+    return out.str();
+}
+
+void Graph::to_file(const std::string &path) {
+    std::ofstream file(path, std::ios_base::out);
+    file << to_string();
+}
--- src/graphviz.h
+++ src/graphviz.h
@@ -2,23 +2,33 @@
 #define GRAPHVIZPP_LIBRARY_H
 
 #include <string>
+#include <utility>
 #include <vector>
+#include <map>
 
-namespace graphivizpp{
+namespace graphvizpp{
+    class Node;
+    class Graph;
 
     class Edge{
-        std::string id;
-        std::string label;
-        unsigned double damping = 0.99;
-        unsigned double k = 0.3;
-        std::string url = "";
+        friend class Graph;
+        std::string from, to;
+        std::map<std::string, std::string> attributes;
+
+    public:
+        Edge(std::string from, std::string to) :
+                from(std::move(from)), to(std::move(to)) {}
     };
 
 
     class Node{
+        friend class Graph;
         std::string id;
-        std::string label;
-        std::vector<Edge> edges;
+        std::map<std::string, std::string> attributes;
+
+    public:
+        explicit Node(std::string id) :
+                id(std::move(id)) {}
     };
 
 
@@ -26,8 +36,24 @@ namespace graphivizpp{
         bool directed;
         bool strict;
         std::string id;
-        std::string label;
-        std::vector<Node> nodes;
+        std::vector<Node*> nodes;
+        std::vector<Edge*> edges;
+
+    public:
+        std::map<std::string, std::string> graph_attributes;
+        std::map<std::string, std::string> node_attributes;
+        std::map<std::string, std::string> edge_attributes;
+
+        explicit Graph(bool directed, bool strict = false, std::string id = "") :
+                directed(directed), strict(strict), id(std::move(id)) {}
+
+        Node* add_node(std::string id);
+
+        Edge* add_edge(std::string from, std::string to);
+
+        std::string to_string();
+
+        void to_file(const std::string &path);
     };
 
 }
